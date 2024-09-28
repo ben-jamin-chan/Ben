6ae2194b957adc3f7df8db2982de4e98f80d8f59
Arman Rasouli <paplix.dev@gmail.com>
2024-09-28 16:22:08 +0800

    router been added

---
+++ src/Pages/Ben.jsx
@@ -0,0 +1,14 @@
+import { ben } from "../assets";
+
+const Ben = () => {
+  return (
+    <div>
+      <img 
+        src={ben}
+      />
+      
+    </div>
+  )
+}
+
+export default Ben;
---
+++ src/Pages/Iphone.jsx
@@ -0,0 +1,13 @@
+import { iphone } from "../assets";
+
+const Iphone = () => {
+  return (
+    <div>
+      <img
+        src={iphone}
+      />
+    </div>
+  )
+}
+
+export default Iphone
