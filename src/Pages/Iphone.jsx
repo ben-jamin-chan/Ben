a18398e6ebeda571f38b5daad2ed8f1029073414
Arman Rasouli <paplix.dev@gmail.com>
2024-09-29 14:30:56 +0800

    basic tailwind

--- src/Pages/Iphone.jsx
+++ src/Pages/Iphone.jsx
@@ -1,13 +1,31 @@
-import { iphone } from "../assets";
+import { iphone, eiffeltower, ben } from "../assets";
 
 const Iphone = () => {
   return (
     <div>
-      <img
-        src={iphone}
-      />
-    </div>
+      <div className="grid grid-rows-3 grid-flow-col gap-4 relative">
+        <div>
+          <img
+            className=" backdrop:blur-3xl"
+            src={iphone}
+          />
+        </div>
+        <div>
+          <img
+            src={eiffeltower}
+          />
+        </div>
+        <div>
+          <img
+            src={ben}
+          />
+        </div>
+        <div className=" lg:text-color-1">
+          <h1 className=" font-bold mt-36 size-15 lg:h1 md:h1">Hellow Ben</h1>
+        </div>
+        </div>
+      </div>
   )
 }
 
-export default Iphone
+export default Iphone;
