36a47bf5ec7ab7a1911f513ca350f99a9ac7f23c
ben-jamin-chan <chanbenjamin.tl@gmail.com>
2024-10-05 18:15:12 +0800

    Fixed blog images

--- src/Pages/blogs/BlogHero.jsx
+++ src/Pages/blogs/BlogHero.jsx
@@ -2,12 +2,12 @@ import { blogbanner } from "../../assets"
 
 const BlogHero = () => {
   return (
-    <div className=" relative lg:mt-0 lg:mb-[15rem]">
+    <div className=" relative lg:mt-0 lg:mb-[5rem]">
         <img
             className="lg:px-10 w-full h-auto "
             src={blogbanner}
         />
-        <h1 className=" absolute inset-0 flex justify-center items-center h1 font-bold text-green-200 text-9xl ">
+        <h1 className=" absolute inset-0 flex justify-center items-center h1 font-bold text-green-200 text-9xl underline drop-shadow-xl ">
             Blogs
         </h1>
       
